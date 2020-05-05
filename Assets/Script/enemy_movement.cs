1b95dbff783467b0d7e6085af1bbe582627337c6
Pietro <pietroiaia98@gmail.com>
2020-05-04 22:38:50 -0400

    Added enemy spawner and movement

---
+++ Assets/Script/enemy_movement.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class enemy_movement : MonoBehaviour
+{
+
+    public int maxSpeed = 4;
+    public int minSpeed = 1;
+    private int speed;
+    // Animator of player
+    public Animator animator;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        speed = Random.Range(minSpeed, maxSpeed);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (speed >= 9) 
+            animator.SetBool("Run Forward", true);
+        else
+            animator.SetBool("Walk Forward", true);
+        
+            
+        this.transform.Translate(-1*Vector3.forward * speed * Time.deltaTime, Space.World);
+
+    }
+}
---
+++ Assets/Script/spawn_enemy.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class spawn_enemy : MonoBehaviour
+{
+    public GameObject enemy;
+    public float minX;
+    public float maxX;
+    public float minZ;
+    public float maxZ;
+    private float Xpos;
+    private float Zpos;
+
+    private void Start()
+    {
+        StartCoroutine(spawner());
+    }
+
+    IEnumerator spawner()
+    {
+        while(true)
+        {
+            Xpos = Random.Range(minX, maxX);
+            Zpos = Random.Range(minZ, maxZ);
+            // Instancia al enemigo, rotado 180 grados en el axis y.
+            Instantiate(enemy, new Vector3(Xpos, -14.73519f, Zpos), Quaternion.Euler(new Vector3(0, 180, 0)));
+            yield return new WaitForSeconds(1f); 
+        }
+    }
+}
