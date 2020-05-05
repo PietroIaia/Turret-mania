918b504031f777aaae599d3826fda103e0855a8c
Pietro <pietroiaia98@gmail.com>
2020-05-05 12:33:58 -0400

    Player takes damage from enemy, scene restart and enemy despawn

---
+++ Assets/Script/Game_state.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;              // This was manually added 
+
+public class Game_state : MonoBehaviour
+{
+    // Player's health
+    private int health = 3;
+    // Player's score
+    private int score = 0;
+
+    public void takeDamage()
+    {
+        health -= 1;
+        // If health drops to zero or below, it restarts the game 
+        if (health <= 0)
+        {
+            SceneManager.LoadScene( SceneManager.GetActiveScene().name );
+        }
+    }
+}
--- Assets/Script/enemy_movement.cs
+++ Assets/Script/enemy_movement.cs
@@ -5,8 +5,8 @@ using UnityEngine;
 public class enemy_movement : MonoBehaviour
 {
 
-    public int maxSpeed = 4;
-    public int minSpeed = 1;
+    public int maxSpeed;
+    public int minSpeed;
     private int speed;
     // Animator of player
     public Animator animator;
@@ -15,18 +15,25 @@ public class enemy_movement : MonoBehaviour
     void Start()
     {
         speed = Random.Range(minSpeed, maxSpeed);
-    }
 
-    // Update is called once per frame
-    void Update()
-    {
         if (speed >= 9) 
             animator.SetBool("Run Forward", true);
         else
             animator.SetBool("Walk Forward", true);
-        
-            
-        this.transform.Translate(-1*Vector3.forward * speed * Time.deltaTime, Space.World);
+    }
 
+    // Update is called once per frame
+    void Update()
+    {          
+        this.transform.Translate(-1*Vector3.forward * speed * Time.deltaTime, Space.World);
+    }
+
+    private void OnTriggerEnter(Collider col)
+    {
+        if (col.name == "Red line")
+            // The player takes damage
+            GameObject.Find("Game Manager").GetComponent<Game_state>().takeDamage();
+            // Destroys the enemy gameObject
+            Destroy(this.gameObject);
     }
 }
